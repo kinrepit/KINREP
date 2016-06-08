2c8b3f062b48041c77afbeef419c7f6acee20d21
Kinrep IR <kinrepit@gmail.com>
2016-06-08 12:35:18 +0000

    Changes made by Kinrep IR (kinrepit@gmail.com).

--- Foster/js/index.js
+++ Foster/js/index.js
@@ -1,50 +1,172 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
+	var that = this,
+		App = new downloadApp();
+		//fileName = "sample.png",
+		//uri = encodeURI("http://www.telerik.com/sfimages/default-source/logos/app_builder.png"),
+		//folderName = "test";
+    
+	//navigator.splashscreen.hide();
+	//App.run(uri, fileName, folderName);
+	App.run();
+}
 
-        console.log('Received Event: ' + id);
-    }
-};
+var downloadApp = function() {
+}
+
+downloadApp.prototype = {
+
+    rst: null,
+
+    run: function () {
+        //rst = document.getElementById("result");
+		var that = this,
+			filePath = "";
+        
+		/*document.getElementById("download").addEventListener("click", function() {
+			that.getFilesystem(
+				function(fileSystem) {
+					console.log("gotFS");
+                    
+					if (device.platform === "Android") {
+						that.getFolder(fileSystem, folderName, function(folder) {
+							filePath = folder.toURL() + "\/" + fileName;
+							that.transferFile(uri, filePath)
+						}, function() {
+							console.log("failed to get folder");
+						});
+					} else {
+						var filePath;
+						var urlPath = fileSystem.root.toURL();
+						if (device.platform == "Win32NT") {
+							urlPath = fileSystem.root.fullPath;
+						}
+						if (parseFloat(device.cordova) <= 3.2) {
+							filePath = urlPath.substring(urlPath.indexOf("/var")) + "\/" + fileName;
+						} else {
+							filePath = urlPath + "\/" + fileName;
+						}
+						that.transferFile(uri, filePath)
+					}
+				},
+				function() {
+					console.log("failed to get filesystem");
+				}
+				);
+		});*/
+		
+		document.getElementById("uploadcheque").addEventListener("click", that.uploadFile);
+		document.getElementById("uploaddoorlock").addEventListener("click", that.uploadFile);
+	},
+    
+	getFilesystem:function (success, fail) {
+		window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, success, fail);
+	},
+
+	getFolder: function (fileSystem, folderName, success, fail) {
+		fileSystem.root.getDirectory(folderName, {create: true, exclusive: false}, success, fail)
+	},
+
+	transferFile: function (uri, filePath) {
+		var transfer = new FileTransfer();
+		transfer.download(
+			uri,
+			filePath,
+			function(entry) {
+				var targetPath = entry.toURL();
+				if (device.platform == "Win32NT") {
+					targetPath = entry.fullPath;
+				}
+				var image = document.getElementById("downloadedImage");
+				image.src = targetPath;
+				image.style.display = "block";
+				image.display = targetPath;
+				//rst.innerHTML = "File saved to: " + targetPath;
+			},
+			function(error) {
+			   // rst.innerHTML = "An error has occurred: Code = " + error.code;
+				console.log("download error source " + error.source);
+				console.log("download error target " + error.target);
+				console.log("upload error code" + error.code);
+			}
+			);
+	},
+	
+	uploadFile: function () {
+        rst = document.getElementById(this.id + 'res');
+	    rst.innerHTML = "";
+		navigator.camera.getPicture(
+			uploadPhoto,
+			function(message) {
+			    rst.innerHTML = "Failed to get a picture. Please select one.";
+			}, {
+				quality         : 50,
+				destinationType : navigator.camera.DestinationType.FILE_URI,
+				sourceType      : navigator.camera.PictureSourceType.PHOTOLIBRARY
+			});
+		
+		function uploadPhoto(fileURI) {
+			var options = new FileUploadOptions();
+			options.fileKey = "file";
+			options.fileName = fileURI.substr(fileURI.lastIndexOf('/') + 1);
+			
+			if (cordova.platformId == "android") {
+				options.fileName += ".jpg" 
+			}
+			
+			options.mimeType = "image/jpeg";
+			options.params = {}; // if we need to send parameters to the server request 
+            
+			options.headers = {
+				Connection: "Close"
+			};
+            //options.httpMethod = 'POST';
+			options.chunkedMode = false;
+
+			var ft = new FileTransfer();
+
+			rst.innerHTML = "Upload in progress...";
+			ft.upload(
+				fileURI,
+				encodeURI("https://www.kinrep.com/foster/ws/filehandler.ashx"),
+				onFileUploadSuccess,
+				onFileTransferFail,
+				options);
+		
+			function onFileUploadSuccess (result) {
+               // rst.innerHTML = "Upload successful";
+				console.log("FileTransfer.upload");
+				console.log("Code = " + result.responseCode);
+				console.log("Response = " + result.response);
+				console.log("Sent = " + result.bytesSent);
+				console.log("Link to uploaded file: https://www.kinrep.com/foster/ws/contentlibrary" + result.response);
+				var response = result.response;
+				var destination = "https://www.kinrep.com/foster/WS/ContentLibrary" + response.substr(response.lastIndexOf('=') + 1);
+                if(this.id == 'uploadcheque') {
+                    document.getElementById("hdnchequeimgpath").value = destination;
+                    
+                } else if(this.id == 'uploaddoorlocked') {
+                    
+                    document.getElementById("hdndoorlockedimgpath").value = destination;
+                } else {
+                    
+                    document.getElementById("hdnothersimgpath").value = destination;
+                }
+				rst.innerHTML = "File uploaded to: " +
+															  destination + 
+															  "</br><button class=\"button\" onclick=\"window.open('" + destination + "', '_blank', 'location=yes')\">Open Location</button>";
+				//document.getElementById("downloadedImage").style.display="none";
+			}
+        
+			function onFileTransferFail (error) {
+                rst.innerHTML = "File Transfer failed: " + error.code;
+				console.log("FileTransfer Error:");
+				console.log("Code: " + error.code);
+				console.log("Source: " + error.source);
+				console.log("Target: " + error.target);
+			}
+		}
+	}
+}
